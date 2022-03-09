f7920fc2e10090c35916b0c8e2d5fada51312dd4
yogeshchaudhary <yogesh.chaudhary@algoworks.com>
2022-03-10 00:55:46 +0530

    linked list impl

--- app/src/main/java/com/example/quotesfullmvvm/model/Result.kt
+++ app/src/main/java/com/example/quotesfullmvvm/model/Result.kt
@@ -1,14 +1,19 @@
 package com.example.quotesfullmvvm.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "quotes")
 data class Result(
+
     @PrimaryKey(autoGenerate = true)
     val id: Int,
+
     val _id: String,
+
     val author: String,
+
     val authorSlug: String,
     val content: String,
     val dateAdded: String,
---
+++ app/src/test/java/MyLinkedList.kt
@@ -0,0 +1,113 @@
+class MyLinkedList<T> {
+    private var head: Node<T>? = null
+    private var size: Int = 0
+
+    class Node<T>(var data: T, var next: Node<T>?)
+
+    private fun addFirst(data: T) {
+        val newNode = Node(data = data, next = null)
+        size++
+        if (head == null) {
+            head = newNode
+            return
+        }
+        newNode.next = head
+        head = newNode
+    }
+
+    private fun addLast(data: T) {
+        val newNode = Node(data = data, next = null)
+        var currNode = head
+        size++
+        if (currNode == null) {
+            head = newNode
+            return
+        }
+        while (currNode?.next != null) {
+            currNode = currNode.next
+        }
+        currNode?.next = newNode
+
+    }
+
+    private fun add(data: T) {
+        addLast(data)
+    }
+
+    private fun printList() {
+        var currentNode = head
+        while (currentNode != null) {
+            print("${currentNode.data} -> ")
+            currentNode = currentNode.next
+        }
+
+        println("NULL ($size)")
+
+    }
+
+    private fun deleteFirst() {
+        println("Delete first")
+        if (head == null || head?.next == null) {
+            head = null
+            size = 0
+            return
+        }
+        size--
+        head = head?.next
+    }
+
+    private fun deleteLast() {
+        println("Delete last")
+        if (head == null || head?.next == null) {
+            head = null
+            size = 0
+            return
+        }
+        var prevNode = head
+        var currNode = head?.next
+        while (currNode?.next != null) {
+            prevNode = currNode
+            currNode = currNode.next
+        }
+        size--
+        prevNode?.next = null
+    }
+
+    private fun get(i: Int): Node<T>? {
+        var pointer = 0;
+        if (i < 0 || i >= size) return null
+
+        var currentNode = head
+        while (pointer < i) {
+            currentNode = currentNode?.next
+            pointer++
+        }
+
+        return currentNode
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun main(str: Array<String>) {
+            val list = MyLinkedList<Int>()
+
+            list.addFirst(1)
+            list.addFirst(2)
+            list.addFirst(3)
+            list.addLast(4)
+            list.printList()
+            list.deleteFirst()
+            list.printList()
+            list.deleteLast()
+            list.printList()
+
+            list.get(-1)?.let { println(it.data) }
+            list.get(1)?.let { println(it.data) }
+            list.get(2)?.let { println(it.data) }
+        }
+
+    }
+
+
+}
